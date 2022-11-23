53a8cff15ee5cb7cf32aeeaa7da80cc9decffa78
Todd Harvey <tharveyster@gmail.com>
2022-11-23 04:04:56 -0500

    Added basic form components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,38 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="from_name"
+            placeholder="Name"
+            required
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            name="user_email"
+            placeholder="Email"
+            required
+          />
+        </label>
+        <label>
+          Message:
+          <textarea
+            type="text"
+            name="message"
+            placeholder="Message"
+            required
+          />
+        </label>
+        <button type="submit">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
